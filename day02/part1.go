199196591e51a70b2543587d81c0404172756707
Kristopher Kirkland <asciiduck@gmail.com>
2021-12-02 19:47:21 -0600

    Add day02's solutions

---
+++ day02/part1.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type submarine struct {
+	distance int
+	depth    int
+}
+
+func (s *submarine) forward(n int) {
+	s.distance += n
+}
+
+func (s *submarine) down(n int) {
+	s.depth += n
+}
+
+func (s *submarine) up(n int) {
+	s.depth -= n
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("No enough command line arguments\n")
+	}
+	file_path := os.Args[1]
+	instructions, err := os.ReadFile(file_path)
+	if err != nil {
+		panic(err)
+	}
+	sub := submarine{0, 0}
+	for _, line := range strings.Split(string(instructions), "\n") {
+		components := strings.Split(line, " ")
+		if len(components) < 2 {
+			fmt.Printf("Line '%s' contained too few components\n", string(line))
+			continue
+		}
+		direction := components[0]
+		magnitude, err := strconv.Atoi(components[1])
+		if err != nil {
+			fmt.Printf("Failed to convert magnitude. '%s' is not numeric\n", components[1])
+
+		}
+
+		if direction == "forward" {
+			sub.forward(magnitude)
+		}
+		if direction == "down" {
+			sub.down(magnitude)
+		}
+		if direction == "up" {
+			sub.up(magnitude)
+		}
+	}
+
+	fmt.Printf("Sub ended at %d, %d\n", sub.distance, sub.depth)
+	fmt.Printf("Final answer: %d\n", sub.distance*sub.depth)
+}
