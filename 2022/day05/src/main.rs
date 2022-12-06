a90bbf13cd1269fcbf7be2495e690d9a25ae1a31
Kristopher Kirkland <me@k8r.io>
2022-12-05 20:21:33 -0600

    cargo fmt

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,7 +1,7 @@
 use util::read_input;
 fn main() {
     let data = read_input("input.txt");
-    let lines: Vec<&str> =  data.split("\n").collect::<Vec<&str>>();
+    let lines: Vec<&str> = data.split("\n").collect::<Vec<&str>>();
     let mut initial_state: Vec<&str> = Vec::new();
     let mut directions: Vec<&str> = Vec::new();
     let mut break_point_found = false;
@@ -12,14 +12,18 @@ fn main() {
         }
         if break_point_found {
             directions.push(line);
-        }
-        else {
+        } else {
             initial_state.push(line);
         }
     }
     let mut reverse_is_iter = initial_state.into_iter().rev();
-    let num_stacks = reverse_is_iter.next().unwrap().split_whitespace().collect::<Vec<&str>>().len();
-    let mut cur_state: Vec<Vec<&str>> = vec![ Vec::new(); num_stacks];
+    let num_stacks = reverse_is_iter
+        .next()
+        .unwrap()
+        .split_whitespace()
+        .collect::<Vec<&str>>()
+        .len();
+    let mut cur_state: Vec<Vec<&str>> = vec![Vec::new(); num_stacks];
     for line in reverse_is_iter {
         let parts = chunker(line);
         for i in 0..parts.len() {
@@ -32,39 +36,46 @@ fn main() {
 
     part1(cur_state.clone(), directions.clone());
     part2(cur_state.clone(), directions.clone());
-
 }
 
 fn part1(mut cur_state: Vec<Vec<&str>>, directions: Vec<&str>) {
     for dir in directions {
         let parts: Vec<&str> = dir.split(" ").collect();
         let num_to_move: i32 = parts[1].parse().unwrap();
-        let src_stack: usize = parts[3].parse::<usize>().unwrap()-1;
-        let dst_stack: usize= parts[5].parse::<usize>().unwrap()-1;
+        let src_stack: usize = parts[3].parse::<usize>().unwrap() - 1;
+        let dst_stack: usize = parts[5].parse::<usize>().unwrap() - 1;
         for _ in 0..num_to_move {
             match cur_state[src_stack].pop() {
                 Some(c) => cur_state[dst_stack].push(c),
-                None => ()
+                None => (),
             }
         }
     }
-    
-    let output = cur_state.into_iter().map(|stack| stack.last().copied().unwrap()).collect::<Vec<&str>>().join("");
+
+    let output = cur_state
+        .into_iter()
+        .map(|stack| stack.last().copied().unwrap())
+        .collect::<Vec<&str>>()
+        .join("");
     println!("part 1 {}", output);
 }
 
 fn part2(mut cur_state: Vec<Vec<&str>>, directions: Vec<&str>) {
     for dir in directions {
         let parts: Vec<&str> = dir.split(" ").collect();
-        let num_to_move: usize= parts[1].parse().unwrap();
-        let src_stack: usize = parts[3].parse::<usize>().unwrap()-1;
-        let dst_stack: usize= parts[5].parse::<usize>().unwrap()-1;
-        let src_stack_len =cur_state[src_stack].len(); 
-        let mut moving_crates = cur_state[src_stack].split_off( src_stack_len - num_to_move);
+        let num_to_move: usize = parts[1].parse().unwrap();
+        let src_stack: usize = parts[3].parse::<usize>().unwrap() - 1;
+        let dst_stack: usize = parts[5].parse::<usize>().unwrap() - 1;
+        let src_stack_len = cur_state[src_stack].len();
+        let mut moving_crates = cur_state[src_stack].split_off(src_stack_len - num_to_move);
         cur_state[dst_stack].append(&mut moving_crates);
     }
-    
-    let output = cur_state.into_iter().map(|stack| stack.last().copied().unwrap()).collect::<Vec<&str>>().join("");
+
+    let output = cur_state
+        .into_iter()
+        .map(|stack| stack.last().copied().unwrap())
+        .collect::<Vec<&str>>()
+        .join("");
     println!("part 2 {}", output);
 }
 
@@ -72,12 +83,11 @@ fn chunker(input: &str) -> Vec<&str> {
     let mut res: Vec<&str> = Vec::new();
     let width = 4;
     let mut i = 0;
-    while i+width < input.len() {
-        res.push(&input[i..i+width]);
-        i+=width;
+    while i + width < input.len() {
+        res.push(&input[i..i + width]);
+        i += width;
     }
     res.push(&input[i..]);
 
     return res;
 }
-
