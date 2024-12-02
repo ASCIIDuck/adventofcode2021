4a68d0a128b8f1af3c45760b118c8852a35a1e53
Kristopher Kirkland <me@k8r.io>
2024-12-01 20:21:20 -0600

    Add 2024 boilerplate

---
+++ 2024/pyaoc24/main.py
@@ -0,0 +1,31 @@
+import argparse
+import importlib
+import traceback
+
+from util.input import get_input
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--day", type=int)
+    args = parser.parse_args()
+
+    day_mod = importlib.import_module("days.day%02d" % args.day)
+    puzzle = get_input(2023, args.day)
+    part_a, part_b = "", ""
+    try:
+        part_a = day_mod.partA(puzzle)
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+    try:
+        part_b = day_mod.partB(puzzle)
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
+    print("Part A: ", part_a)
+    print("Part B: ", part_b)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/pyaoc24/util/input.py
@@ -0,0 +1,35 @@
+import requests
+
+
+def transform_input(transformer: callable, args: list = []) -> callable:
+    def param_wrapper(decorated_function: callable) -> callable:
+        def puzzle_parse(puzzle: str) -> int:
+            parsed_args = transformer(puzzle, *args)
+            return decorated_function(*parsed_args)
+
+        return puzzle_parse
+
+    return param_wrapper
+
+
+def get_token() -> str:
+    try:
+        with open(".token", "r") as f:
+            return f.read().strip()
+    except:
+        return ""
+
+
+def get_input(year: int, day: int) -> str:
+    cache_path = ".cache/day%02d.input" % day
+    try:
+        with open(cache_path, "+r") as f:
+            return f.read().strip()
+    except FileNotFoundError:
+        url = "https://adventofcode.com/%d/day/%d/input" % (year, day)
+        sess = requests.Session()
+        resp = sess.get(url, cookies={"session": get_token()})
+        puzzle_input = resp.content.decode()
+        with open(cache_path, "+w") as f:
+            f.write(puzzle_input)
+        return puzzle_input
